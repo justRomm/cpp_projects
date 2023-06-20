2899eb5c0f91828a8a3dc4835c4f92854407f316
justRomm <126328656+justRomm@users.noreply.github.com>
2023-06-20 10:13:57 +0300

    Update Task.cpp

--- course_work_semestr_4/Code/Task.cpp
+++ course_work_semestr_4/Code/Task.cpp
@@ -1,35 +1,34 @@
-/* БС-14 Романюк М.Д.
- * Реалізація методів класу Task у файлі Task.cpp*/
+
 
 #include "Task.h"
 // -------------------------------------------------------------------------------------------------
-// Конструктор за замовчуванням
+// ГЉГ®Г­Г±ГІГ°ГіГЄГІГ®Г° Г§Г  Г§Г Г¬Г®ГўГ·ГіГўГ Г­Г­ГїГ¬
 Task::Task()
 	: Assignment()
 	, taskDesc("")
 {}
 // -------------------------------------------------------------------------------------------------
-// Парметризований конструктор
+// ГЏГ Г°Г¬ГҐГІГ°ГЁГ§Г®ГўГ Г­ГЁГ© ГЄГ®Г­Г±ГІГ°ГіГЄГІГ®Г°
 Task::Task(int _id, int _priority, std::string _component, std::string _taskDesc)
 	: Assignment(_id, _priority, _component)
 	, taskDesc(_taskDesc)
 {}
 // -------------------------------------------------------------------------------------------------
-// Сеттер опису завдання
+// Г‘ГҐГІГІГҐГ° Г®ГЇГЁГ±Гі Г§Г ГўГ¤Г Г­Г­Гї
 void Task::setTaskDesc(std::string _taskDesc) { taskDesc = _taskDesc; }
 // -------------------------------------------------------------------------------------------------
-// Геттер опису завдання
+// ГѓГҐГІГІГҐГ° Г®ГЇГЁГ±Гі Г§Г ГўГ¤Г Г­Г­Гї
 std::string Task::getTaskDesc() const { return taskDesc; }
 // -------------------------------------------------------------------------------------------------
-// Перевизначений метод для створення завдання з клавіатури
+// ГЏГҐГ°ГҐГўГЁГ§Г­Г Г·ГҐГ­ГЁГ© Г¬ГҐГІГ®Г¤ Г¤Г«Гї Г±ГІГўГ®Г°ГҐГ­Г­Гї Г§Г ГўГ¤Г Г­Г­Гї Г§ ГЄГ«Г ГўВіГ ГІГіГ°ГЁ
 void Task::createFromKeyboard()
 {
-	// Взяття значення ID з вводу користувача
+	// Г‚Г§ГїГІГІГї Г§Г­Г Г·ГҐГ­Г­Гї ID Г§ ГўГўГ®Г¤Гі ГЄГ®Г°ГЁГ±ГІГіГўГ Г·Г 
 	std::cout << "Enter ID: ";
 	std::string inp;
 	std::cin >> inp;
 	id = atoi(inp.c_str());
-	// Перевірка на коректність вводу
+	// ГЏГҐГ°ГҐГўВіГ°ГЄГ  Г­Г  ГЄГ®Г°ГҐГЄГІГ­ВіГ±ГІГј ГўГўГ®Г¤Гі
 	while (id < 1) 
 	{
 		std::cout << "ID must be a number greater then 0.\nEnter again: ";
@@ -37,11 +36,11 @@ void Task::createFromKeyboard()
 		std::cin >> inp;
 		id = atoi(inp.c_str());
 	}
-	// Взяття значення пріоритету з вводу користувача
+	// Г‚Г§ГїГІГІГї Г§Г­Г Г·ГҐГ­Г­Гї ГЇГ°ВіГ®Г°ГЁГІГҐГІГі Г§ ГўГўГ®Г¤Гі ГЄГ®Г°ГЁГ±ГІГіГўГ Г·Г 
 	std::cout << "Enter priority: ";
 	std::cin >> inp;
 	priority = atoi(inp.c_str());
-	// Перевірка на коректність вводу
+	// ГЏГҐГ°ГҐГўВіГ°ГЄГ  Г­Г  ГЄГ®Г°ГҐГЄГІГ­ВіГ±ГІГј ГўГўГ®Г¤Гі
 	while (priority < 1)
 	{
 		std::cout << "Priority must be a number greater then 0.\nEnter again: ";
@@ -49,20 +48,20 @@ void Task::createFromKeyboard()
 		std::cin >> inp;
 		priority = atoi(inp.c_str());
 	}
-	// Взяття значення назви компоненту з вводу користувача
+	// Г‚Г§ГїГІГІГї Г§Г­Г Г·ГҐГ­Г­Гї Г­Г Г§ГўГЁ ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГі Г§ ГўГўГ®Г¤Гі ГЄГ®Г°ГЁГ±ГІГіГўГ Г·Г 
 	std::cout << "Enter component: ";
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	std::getline(std::cin, component, '\n');
-	// Перевірка на коректність вводу
+	// ГЏГҐГ°ГҐГўВіГ°ГЄГ  Г­Г  ГЄГ®Г°ГҐГЄГІГ­ВіГ±ГІГј ГўГўГ®Г¤Гі
 	while (component.empty())
 	{
 		std::cout << "Component's name can not be an empty string.\nEnter again: ";
 		std::getline(std::cin, component, '\n');
 	}
-	// Взяття значення опису завдання з вводу користувача
+	// Г‚Г§ГїГІГІГї Г§Г­Г Г·ГҐГ­Г­Гї Г®ГЇГЁГ±Гі Г§Г ГўГ¤Г Г­Г­Гї Г§ ГўГўГ®Г¤Гі ГЄГ®Г°ГЁГ±ГІГіГўГ Г·Г 
 	std::cout << "Enter desc: ";
 	std::getline(std::cin, taskDesc, '\n');
-	// Перевірка на коректність вводу
+	// ГЏГҐГ°ГҐГўВіГ°ГЄГ  Г­Г  ГЄГ®Г°ГҐГЄГІГ­ВіГ±ГІГј ГўГўГ®Г¤Гі
 	while (taskDesc.empty())
 	{
 		std::cout << "Task description can not be an empty string.\nEnter again: ";
@@ -70,79 +69,79 @@ void Task::createFromKeyboard()
 	}
 }
 // -------------------------------------------------------------------------------------------------
-// Перевизначений метод для виводу завдання в потік
+// ГЏГҐГ°ГҐГўГЁГ§Г­Г Г·ГҐГ­ГЁГ© Г¬ГҐГІГ®Г¤ Г¤Г«Гї ГўГЁГўГ®Г¤Гі Г§Г ГўГ¤Г Г­Г­Гї Гў ГЇГ®ГІВіГЄ
 void Task::show() const 
 {
 	Assignment::show();
 	std::cout << "Task description: " << taskDesc << "\n\n";
 }
 // -------------------------------------------------------------------------------------------------
-// Перевантажена дружня функція для серіалізації об'єкта завдання до потоку
+// ГЏГҐГ°ГҐГўГ Г­ГІГ Г¦ГҐГ­Г  Г¤Г°ГіГ¦Г­Гї ГґГіГ­ГЄГ¶ВіГї Г¤Г«Гї Г±ГҐГ°ВіГ Г«ВіГ§Г Г¶ВіВї Г®ГЎ'ВєГЄГІГ  Г§Г ГўГ¤Г Г­Г­Гї Г¤Г® ГЇГ®ГІГ®ГЄГі
 std::ostream& operator<<(std::ostream& out, const Task& task)
 {
-	// Запис ID та пріоритет до вихідного файлового потоку
+	// Г‡Г ГЇГЁГ± ID ГІГ  ГЇГ°ВіГ®Г°ГЁГІГҐГІ Г¤Г® ГўГЁГµВіГ¤Г­Г®ГЈГ® ГґГ Г©Г«Г®ГўГ®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	out.write((char*)&task.id, sizeof(task.id));
 	out.write((char*)&task.priority, sizeof(task.priority));
-	// Запис розміру рядка назви компоненту та сам рядок до вихідного потоку
+	// Г‡Г ГЇГЁГ± Г°Г®Г§Г¬ВіГ°Гі Г°ГїГ¤ГЄГ  Г­Г Г§ГўГЁ ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГі ГІГ  Г±Г Г¬ Г°ГїГ¤Г®ГЄ Г¤Г® ГўГЁГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	size_t componentSize = task.component.size();
 	out.write((char*)&componentSize, sizeof(componentSize));
 	out.write(task.component.data(), componentSize);
-	// Запис розміру рядка опису завдання та сам рядок до вихідного потоку
+	// Г‡Г ГЇГЁГ± Г°Г®Г§Г¬ВіГ°Гі Г°ГїГ¤ГЄГ  Г®ГЇГЁГ±Гі Г§Г ГўГ¤Г Г­Г­Гї ГІГ  Г±Г Г¬ Г°ГїГ¤Г®ГЄ Г¤Г® ГўГЁГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	size_t taskDescSize = task.taskDesc.size();
 	out.write((char*)&taskDescSize, sizeof(taskDescSize));
 	out.write(task.taskDesc.data(), taskDescSize);
-	// Повернення вихідного потоку
+	// ГЏГ®ГўГҐГ°Г­ГҐГ­Г­Гї ГўГЁГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	return out;
 }
 // -------------------------------------------------------------------------------------------------
-// Перевантажена дружня функція для десеріалізації об'єкта завдання з вхідного потоку
+// ГЏГҐГ°ГҐГўГ Г­ГІГ Г¦ГҐГ­Г  Г¤Г°ГіГ¦Г­Гї ГґГіГ­ГЄГ¶ВіГї Г¤Г«Гї Г¤ГҐГ±ГҐГ°ВіГ Г«ВіГ§Г Г¶ВіВї Г®ГЎ'ВєГЄГІГ  Г§Г ГўГ¤Г Г­Г­Гї Г§ ГўГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 std::ifstream& operator>>(std::ifstream& in, Task* task)
 {
 	try {
-		// Зчитування ID з вхідного потоку
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї ID Г§ ГўГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 		in.read((char*)&task->id, sizeof(task->id));
 		if (in.fail()) {
 			throw std::runtime_error("Error reading task ID");
 		}
-		// Зчитування пріоритету з вхідного потоку
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї ГЇГ°ВіГ®Г°ГЁГІГҐГІГі Г§ ГўГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 		in.read((char*)&task->priority, sizeof(task->priority));
 		if (in.fail()) {
 			throw std::runtime_error("Error reading task priority");
 		}
-		// Зчитування розміру рядка назви компонента
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї Г°Г®Г§Г¬ВіГ°Гі Г°ГїГ¤ГЄГ  Г­Г Г§ГўГЁ ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГ 
 		size_t componentSize;
 		in.read((char*)&componentSize, sizeof(componentSize));
 		if (in.fail()) {
 			throw std::runtime_error("Error reading component size");
 		}
-		// Зчитування рядка назви компонента
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї Г°ГїГ¤ГЄГ  Г­Г Г§ГўГЁ ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГ 
 		task->component.resize(componentSize);
 		in.read((char*)task->component.data(), componentSize);
 		if (in.fail()) {
 			throw std::runtime_error("Error reading component's name data");
 		}
-		// Зчитування розміру рядка опису завдання
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї Г°Г®Г§Г¬ВіГ°Гі Г°ГїГ¤ГЄГ  Г®ГЇГЁГ±Гі Г§Г ГўГ¤Г Г­Г­Гї
 		size_t taskDescSize;
 		in.read((char*)&taskDescSize, sizeof(taskDescSize));
 		if (in.fail()) {
 			throw std::runtime_error("Error reading task description size");
 		}
-		// Зчитування рядка опису завдання
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї Г°ГїГ¤ГЄГ  Г®ГЇГЁГ±Гі Г§Г ГўГ¤Г Г­Г­Гї
 		task->taskDesc.resize(taskDescSize);
 		in.read((char*)task->taskDesc.data(), taskDescSize);
 		if (in.fail()) {
 			throw std::runtime_error("Error reading task description data");
 		}
-	} // кінець try
-	// Обробка усіх можливих винятків під час десеріалізації об'єкта з вхідного потоку
+	} // ГЄВіГ­ГҐГ¶Гј try
+	// ГЋГЎГ°Г®ГЎГЄГ  ГіГ±ВіГµ Г¬Г®Г¦Г«ГЁГўГЁГµ ГўГЁГ­ГїГІГЄВіГў ГЇВіГ¤ Г·Г Г± Г¤ГҐГ±ГҐГ°ВіГ Г«ВіГ§Г Г¶ВіВї Г®ГЎ'ВєГЄГІГ  Г§ ГўГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	catch (std::exception& e) {
 		std::cout << "Error: " << e.what() << std::endl;
 		in.setstate(std::ios::failbit);
 	}
-	// Повернення вхідного потоку
+	// ГЏГ®ГўГҐГ°Г­ГҐГ­Г­Гї ГўГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	return in;
-} // кінець friend std::ifstream& operator>>(std::ifstream& in, Task* task)
+} // ГЄВіГ­ГҐГ¶Гј friend std::ifstream& operator>>(std::ifstream& in, Task* task)
 // -------------------------------------------------------------------------------------------------
-// Деструктор
+// Г„ГҐГ±ГІГ°ГіГЄГІГ®Г°
 Task::~Task()
 {}
