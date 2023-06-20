47bf8f8b6aa1384edd7899c19990e5d6eb908699
justRomm <126328656+justRomm@users.noreply.github.com>
2023-06-20 10:13:13 +0300

    Update Bug.cpp

--- course_work_semestr_4/Code/Bug.cpp
+++ course_work_semestr_4/Code/Bug.cpp
@@ -1,9 +1,7 @@
-/* БС-14 Романюк М.Д.
- * Реалізація методів класу Bug у файлі Bug.cpp */
 
 #include "Bug.h"
  // -------------------------------------------------------------------------------------------------
-// Конструктор за замовчуванням
+// ГЉГ®Г­Г±ГІГ°ГіГЄГІГ®Г° Г§Г  Г§Г Г¬Г®ГўГ·ГіГўГ Г­Г­ГїГ¬
 Bug::Bug()
 	: Assignment()
 	, expRes("")
@@ -11,7 +9,7 @@ Bug::Bug()
 {
 }
 // -------------------------------------------------------------------------------------------------
-// Парметризований конструктор
+// ГЏГ Г°Г¬ГҐГІГ°ГЁГ§Г®ГўГ Г­ГЁГ© ГЄГ®Г­Г±ГІГ°ГіГЄГІГ®Г°
 Bug::Bug(int _id, int _priority, std::string _component, std::string _expRes, std::string _givenRes)
 	: Assignment(_id, _priority, _component)
 	, expRes(_expRes)
@@ -19,38 +17,38 @@ Bug::Bug(int _id, int _priority, std::string _component, std::string _expRes, st
 {
 }
 // -------------------------------------------------------------------------------------------------
-// Сеттер очікуваних результатів 
+// Г‘ГҐГІГІГҐГ° Г®Г·ВіГЄГіГўГ Г­ГЁГµ Г°ГҐГ§ГіГ«ГјГІГ ГІВіГў 
 void Bug::setExpRes(std::string _expRes) { expRes = _expRes; }
 // -------------------------------------------------------------------------------------------------
-// Сеттер отриманих результатів 
+// Г‘ГҐГІГІГҐГ° Г®ГІГ°ГЁГ¬Г Г­ГЁГµ Г°ГҐГ§ГіГ«ГјГІГ ГІВіГў 
 void Bug::setObtRes(std::string _givenRes) { obtRes = _givenRes; }
 // -------------------------------------------------------------------------------------------------
-// Геттерочікуваних результатів
+// ГѓГҐГІГІГҐГ°Г®Г·ВіГЄГіГўГ Г­ГЁГµ Г°ГҐГ§ГіГ«ГјГІГ ГІВіГў
 std::string Bug::getExpRes() const { return expRes; }
 // -------------------------------------------------------------------------------------------------
-// Геттер отриманих результатів
+// ГѓГҐГІГІГҐГ° Г®ГІГ°ГЁГ¬Г Г­ГЁГµ Г°ГҐГ§ГіГ«ГјГІГ ГІВіГў
 std::string Bug::getObtRes() const { return obtRes; }
 // -------------------------------------------------------------------------------------------------
-// Перевизначений метод для створення дефекту з клавіатури
+// ГЏГҐГ°ГҐГўГЁГ§Г­Г Г·ГҐГ­ГЁГ© Г¬ГҐГІГ®Г¤ Г¤Г«Гї Г±ГІГўГ®Г°ГҐГ­Г­Гї Г¤ГҐГґГҐГЄГІГі Г§ ГЄГ«Г ГўВіГ ГІГіГ°ГЁ
 void Bug::createFromKeyboard()
 {
-	// Взяття значення ID з вводу користувача
+	// Г‚Г§ГїГІГІГї Г§Г­Г Г·ГҐГ­Г­Гї ID Г§ ГўГўГ®Г¤Гі ГЄГ®Г°ГЁГ±ГІГіГўГ Г·Г 
 	std::cout << "Enter ID: ";
 	std::string inp;
 	std::cin >> inp;
 	id = atoi(inp.c_str());
-	// Перевірка на коректність вводу
+	// ГЏГҐГ°ГҐГўВіГ°ГЄГ  Г­Г  ГЄГ®Г°ГҐГЄГІГ­ВіГ±ГІГј ГўГўГ®Г¤Гі
 	while (id < 1)
 	{
 		std::cout << "ID must be a number greater then 0.\nEnter again: ";
 		std::cin >> inp;
 		id = atoi(inp.c_str());
 	}
-	// Взяття значення пріоритету з вводу користувача
+	// Г‚Г§ГїГІГІГї Г§Г­Г Г·ГҐГ­Г­Гї ГЇГ°ВіГ®Г°ГЁГІГҐГІГі Г§ ГўГўГ®Г¤Гі ГЄГ®Г°ГЁГ±ГІГіГўГ Г·Г 
 	std::cout << "Enter priority: ";
 	std::cin >> inp;
 	priority = atoi(inp.c_str());
-	// Перевірка на коректність вводу
+	// ГЏГҐГ°ГҐГўВіГ°ГЄГ  Г­Г  ГЄГ®Г°ГҐГЄГІГ­ВіГ±ГІГј ГўГўГ®Г¤Гі
 	while (priority < 1)
 	{
 		std::cout << "Priority must be a number greater then 0.\nEnter again: ";
@@ -58,29 +56,29 @@ void Bug::createFromKeyboard()
 		std::cin >> inp;
 		priority = atoi(inp.c_str());
 	}
-	// Взяття значення назви компоненту з вводу користувача
+	// Г‚Г§ГїГІГІГї Г§Г­Г Г·ГҐГ­Г­Гї Г­Г Г§ГўГЁ ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГі Г§ ГўГўГ®Г¤Гі ГЄГ®Г°ГЁГ±ГІГіГўГ Г·Г 
 	std::cout << "Enter component: ";
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	std::getline(std::cin, component, '\n');
-	// Перевірка на коректність вводу
+	// ГЏГҐГ°ГҐГўВіГ°ГЄГ  Г­Г  ГЄГ®Г°ГҐГЄГІГ­ВіГ±ГІГј ГўГўГ®Г¤Гі
 	while (component.empty())
 	{
 		std::cout << "Component's name can not be an empty string.\nEnter again: ";
 		std::getline(std::cin, component, '\n');
 	}
-	// Взяття значення очікуваних результатів
+	// Г‚Г§ГїГІГІГї Г§Г­Г Г·ГҐГ­Г­Гї Г®Г·ВіГЄГіГўГ Г­ГЁГµ Г°ГҐГ§ГіГ«ГјГІГ ГІВіГў
 	std::cout << "Enter expected result: ";
 	std::getline(std::cin, expRes, '\n');
-	// Перевірка на коректність вводу
+	// ГЏГҐГ°ГҐГўВіГ°ГЄГ  Г­Г  ГЄГ®Г°ГҐГЄГІГ­ВіГ±ГІГј ГўГўГ®Г¤Гі
 	while (expRes.empty())
 	{
 		std::cout << "Expected result can not be an empty string.\nEnter again: ";
 		std::getline(std::cin, expRes, '\n');
 	}
-	// Взяття значення отриманих результатів
+	// Г‚Г§ГїГІГІГї Г§Г­Г Г·ГҐГ­Г­Гї Г®ГІГ°ГЁГ¬Г Г­ГЁГµ Г°ГҐГ§ГіГ«ГјГІГ ГІВіГў
 	std::cout << "Enter obtained result: ";
 	std::getline(std::cin, obtRes, '\n');
-	// Перевірка на коректність вводу
+	// ГЏГҐГ°ГҐГўВіГ°ГЄГ  Г­Г  ГЄГ®Г°ГҐГЄГІГ­ВіГ±ГІГј ГўГўГ®Г¤Гі
 	while (obtRes.empty())
 	{
 		std::cout << "Obtained result can not be an empty string.\nEnter again: ";
@@ -88,96 +86,96 @@ void Bug::createFromKeyboard()
 	}
 }
 // -------------------------------------------------------------------------------------------------
-// Перевизначений метод для виводу завдання в потік
+// ГЏГҐГ°ГҐГўГЁГ§Г­Г Г·ГҐГ­ГЁГ© Г¬ГҐГІГ®Г¤ Г¤Г«Гї ГўГЁГўГ®Г¤Гі Г§Г ГўГ¤Г Г­Г­Гї Гў ГЇГ®ГІВіГЄ
 void Bug::show() const
 {
 	Assignment::show();
 	std::cout << "Expected result: " << expRes 
 			  << "\nObtained results: " << obtRes << "\n\n";
 }
-// Перевантажена дружня функція для серіалізації об'єкта дефекту до потоку
+// ГЏГҐГ°ГҐГўГ Г­ГІГ Г¦ГҐГ­Г  Г¤Г°ГіГ¦Г­Гї ГґГіГ­ГЄГ¶ВіГї Г¤Г«Гї Г±ГҐГ°ВіГ Г«ВіГ§Г Г¶ВіВї Г®ГЎ'ВєГЄГІГ  Г¤ГҐГґГҐГЄГІГі Г¤Г® ГЇГ®ГІГ®ГЄГі
 std::ostream& operator<<(std::ostream& out, const Bug& bug)
 {
-	// Запис ID та пріоритет до вихідного файлового потоку
+	// Г‡Г ГЇГЁГ± ID ГІГ  ГЇГ°ВіГ®Г°ГЁГІГҐГІ Г¤Г® ГўГЁГµВіГ¤Г­Г®ГЈГ® ГґГ Г©Г«Г®ГўГ®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	out.write((char*)&bug.id, sizeof(bug.id));
 	out.write((char*)&bug.priority, sizeof(bug.priority));
-	// Запис розміру рядка назви компоненту та сам рядок назви компоненту до вихідного потоку
+	// Г‡Г ГЇГЁГ± Г°Г®Г§Г¬ВіГ°Гі Г°ГїГ¤ГЄГ  Г­Г Г§ГўГЁ ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГі ГІГ  Г±Г Г¬ Г°ГїГ¤Г®ГЄ Г­Г Г§ГўГЁ ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГі Г¤Г® ГўГЁГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	size_t componentSize = bug.component.size();
 	out.write((char*)&componentSize, sizeof(componentSize));
 	out.write(bug.component.data(), componentSize);
-	// Запис розміру рядка очікуваних результатів та сам рядок до вихідного потоку
+	// Г‡Г ГЇГЁГ± Г°Г®Г§Г¬ВіГ°Гі Г°ГїГ¤ГЄГ  Г®Г·ВіГЄГіГўГ Г­ГЁГµ Г°ГҐГ§ГіГ«ГјГІГ ГІВіГў ГІГ  Г±Г Г¬ Г°ГїГ¤Г®ГЄ Г¤Г® ГўГЁГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	size_t expResSize = bug.expRes.size();
 	out.write((char*)&expResSize, sizeof(expResSize));
 	out.write(bug.expRes.data(), expResSize);
-	// Запис розміру рядка отриманих результатів та сам рядок до вихідного потоку
+	// Г‡Г ГЇГЁГ± Г°Г®Г§Г¬ВіГ°Гі Г°ГїГ¤ГЄГ  Г®ГІГ°ГЁГ¬Г Г­ГЁГµ Г°ГҐГ§ГіГ«ГјГІГ ГІВіГў ГІГ  Г±Г Г¬ Г°ГїГ¤Г®ГЄ Г¤Г® ГўГЁГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	size_t obtResSize = bug.obtRes.size();
 	out.write((char*)&obtResSize, sizeof(obtResSize));
 	out.write(bug.obtRes.data(), obtResSize);
-	// Повернення вихідного потоку
+	// ГЏГ®ГўГҐГ°Г­ГҐГ­Г­Гї ГўГЁГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	return out;
 }
 // -------------------------------------------------------------------------------------------------
-// Перевантажена дружня функція для десеріалізації об'єкта дефекту з вхідного потоку
+// ГЏГҐГ°ГҐГўГ Г­ГІГ Г¦ГҐГ­Г  Г¤Г°ГіГ¦Г­Гї ГґГіГ­ГЄГ¶ВіГї Г¤Г«Гї Г¤ГҐГ±ГҐГ°ВіГ Г«ВіГ§Г Г¶ВіВї Г®ГЎ'ВєГЄГІГ  Г¤ГҐГґГҐГЄГІГі Г§ ГўГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 std::istream& operator>>(std::istream& in, Bug* bug)
 {
 	try {
-		// Зчитування ID з вхідного потоку
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї ID Г§ ГўГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 		in.read((char*)&bug->id, sizeof(bug->id));
 		if (in.fail()) {
 			throw std::runtime_error("Error reading bug ID");
 		}
-		// Зчитування пріоритету з вхідного потоку
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї ГЇГ°ВіГ®Г°ГЁГІГҐГІГі Г§ ГўГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 		in.read((char*)&bug->priority, sizeof(bug->priority));
 		if (in.fail()) {
 			throw std::runtime_error("Error reading bug priority");
 		}
-		// Зчитування розміру рядка назви компонента
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї Г°Г®Г§Г¬ВіГ°Гі Г°ГїГ¤ГЄГ  Г­Г Г§ГўГЁ ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГ 
 		size_t componentSize;
 		in.read((char*)&componentSize, sizeof(componentSize));
 		if (in.fail()) {
 			throw std::runtime_error("Error reading component size");
 		}
-		// Зчитування розміру рядка очікуваного результату назви компонента
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї Г°Г®Г§Г¬ВіГ°Гі Г°ГїГ¤ГЄГ  Г®Г·ВіГЄГіГўГ Г­Г®ГЈГ® Г°ГҐГ§ГіГ«ГјГІГ ГІГі Г­Г Г§ГўГЁ ГЄГ®Г¬ГЇГ®Г­ГҐГ­ГІГ 
 		bug->component.resize(componentSize);
 		in.read((char*)bug->component.data(), componentSize);
 		if (in.fail()) {
 			throw std::runtime_error("Error reading component data");
 		}
-		// Зчитування розміру рядка очікуваного результату
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї Г°Г®Г§Г¬ВіГ°Гі Г°ГїГ¤ГЄГ  Г®Г·ВіГЄГіГўГ Г­Г®ГЈГ® Г°ГҐГ§ГіГ«ГјГІГ ГІГі
 		size_t expResSize;
 		in.read((char*)&expResSize, sizeof(expResSize));
 		if (in.fail()) {
 			throw std::runtime_error("Error reading expected result size");
 		}
-		// Зчитування рядка очікуваного результату
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї Г°ГїГ¤ГЄГ  Г®Г·ВіГЄГіГўГ Г­Г®ГЈГ® Г°ГҐГ§ГіГ«ГјГІГ ГІГі
 		bug->expRes.resize(expResSize);
 		in.read((char*)bug->expRes.data(), expResSize);
 		if (in.fail()) {
 			throw std::runtime_error("Error reading expected result data");
 		}
-		// Зчитування розміру рядка отриманого результату
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї Г°Г®Г§Г¬ВіГ°Гі Г°ГїГ¤ГЄГ  Г®ГІГ°ГЁГ¬Г Г­Г®ГЈГ® Г°ГҐГ§ГіГ«ГјГІГ ГІГі
 		size_t obtResSize = 0;
 		in.read((char*)&obtResSize, sizeof(obtResSize));
 		if (in.fail()) {
 			throw std::runtime_error("Error reading obtained result size");
 		}
-		// Зчитування рядка отриманого результату
+		// Г‡Г·ГЁГІГіГўГ Г­Г­Гї Г°ГїГ¤ГЄГ  Г®ГІГ°ГЁГ¬Г Г­Г®ГЈГ® Г°ГҐГ§ГіГ«ГјГІГ ГІГі
 		bug->obtRes.resize(obtResSize);
 		in.read((char*)bug->obtRes.data(), obtResSize);
 		if (in.fail()) {
 			throw std::runtime_error("Error reading obtained result data");
 		}
-	} // кінець try
-	// Обробка усіх можливих винятків під час десеріалізації об'єкта з вхідного потоку
+	} // ГЄВіГ­ГҐГ¶Гј try
+	// ГЋГЎГ°Г®ГЎГЄГ  ГіГ±ВіГµ Г¬Г®Г¦Г«ГЁГўГЁГµ ГўГЁГ­ГїГІГЄВіГў ГЇВіГ¤ Г·Г Г± Г¤ГҐГ±ГҐГ°ВіГ Г«ВіГ§Г Г¶ВіВї Г®ГЎ'ВєГЄГІГ  Г§ ГўГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	catch (std::exception& e) {
 		std::cerr << "Error: " << e.what() << std::endl;
 		in.setstate(std::ios::failbit);
 	}
-	// Повернення вхідного потоку
+	// ГЏГ®ГўГҐГ°Г­ГҐГ­Г­Гї ГўГµВіГ¤Г­Г®ГЈГ® ГЇГ®ГІГ®ГЄГі
 	return in;
-} // кінець friend std::istream& operator>>(std::istream& in, Bug* bug)
+} // ГЄВіГ­ГҐГ¶Гј friend std::istream& operator>>(std::istream& in, Bug* bug)
 // -------------------------------------------------------------------------------------------------
-// Деструктор
+// Г„ГҐГ±ГІГ°ГіГЄГІГ®Г°
 Bug::~Bug()
 {
 }
